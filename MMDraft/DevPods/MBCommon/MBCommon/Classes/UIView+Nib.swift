ab14bf9a27f9e7a39a49fa2ef33dd517ae2b9287
Zhenis Mutan <zhenis.iitu@gmail.com>
2021-04-19 02:26:16 +0600

    configured uinib to class

--- MMDraft/DevPods/MBCommon/MBCommon/Classes/UIView+Nib.swift
+++ MMDraft/DevPods/MBCommon/MBCommon/Classes/UIView+Nib.swift
@@ -7,40 +7,16 @@
 
 import UIKit
 
-public protocol NibLoadableProtocol: class {
-    static var nib: UINib { get }
-    static var nibName: String { get }
-    
-    static func loadFromNib() -> Self?
-}
-
-public extension NibLoadableProtocol {
-    static var nibName: String {
-        return String(describing: self)
-    }
-
-    static var nib: UINib {
-        return UINib(nibName: nibName, bundle: Bundle(for: self))
+public extension UIView {
+    func loadFromNib(_ bundle: Bundle? = Bundle.main) {
+            let className = String(describing: type(of: self))
+            guard let view = bundle?.loadNibNamed(className,
+                                                  owner: self,
+                                                  options: nil)?.first as? UIView else {
+                                                    fatalError("No xib file found for UIView object: \(self)")
+            }
+            
+            view.frame = bounds
+            addSubview(view)
     }
 }
-
-public extension NibLoadableProtocol where Self: UIView {
-    static func loadFromNib() -> Self? {
-        return nib.instantiate(withOwner: nil, options: nil).first as? Self
-    }
-}
-
-public extension NibLoadableProtocol where Self: UIViewController {
-    @discardableResult
-    static func loadFromNib() -> Self? {
-        return self.init(nibName: nibName, bundle: Bundle(for: self))
-    }
-}
-
-public protocol UniqNibLoadableProtocol: class {
-    static var uniqNibName: String { get }
-    static var uniqNib: UINib { get }
-}
-
-extension UIView: NibLoadableProtocol {}
-extension UIViewController: NibLoadableProtocol {}
